432961ab1ec2fe7f04f18aee87c727768b4ebdf3
Ben Pearson <ben@rdydev.com>
2014-08-28 19:02:35 +0100

    raw project template

---
+++ cat-walk/src/net/rdyonline/catwalk/DisplayCatActivity.java
@@ -0,0 +1,16 @@
+package net.rdyonline.catwalk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DisplayCatActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_display_cat);
+
+	}
+
+}
