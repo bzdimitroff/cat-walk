0dc43df2863fe79806bc43686c4691db0b9dc56c
Ben Pearson <ben@rdydev.com>
2014-08-29 08:59:57 +0100

    Handle network state

--- cat-walk/src/net/rdyonline/catwalk/DisplayCatActivity.java
+++ cat-walk/src/net/rdyonline/catwalk/DisplayCatActivity.java
@@ -7,12 +7,18 @@ import net.rdyonline.catwalk.data.api.cat.CatApi;
 import net.rdyonline.catwalk.tasks.SafeASyncTask;
 import net.rdyonline.catwalk.ui.RoundedTransformation;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -27,7 +33,40 @@ public class DisplayCatActivity extends Activity {
 	private int mPositionInPage = 0;
 
 	private ViewGroup mLoadingContainer;
+	private TextView mLoadingText;
 	private ImageView mCatImage;
+
+	private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if (!isConnected()) {
+				mCatImage.setVisibility(View.INVISIBLE);
+				mLoadingContainer.setVisibility(View.VISIBLE);
+				mLoadingText.setText(context.getText(R.string.no_network));
+			} else {
+				mLoadingText.setText(context.getText(R.string.loading));
+			}
+		}
+	};
+
+	/**
+	 * Without a network connection the app doesn't work at all, so the state
+	 * needs to be determined so we know whether we can load cats
+	 * 
+	 * @return true if either WiFi or mobile data is available
+	 */
+	private boolean isConnected() {
+		ConnectivityManager conMngr = (ConnectivityManager) this
+				.getSystemService(Activity.CONNECTIVITY_SERVICE);
+		NetworkInfo wifi = conMngr
+				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		NetworkInfo mobile = conMngr
+				.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+		return (null != wifi && wifi.isConnected())
+				|| (null != mobile && mobile.isConnected());
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +75,19 @@ public class DisplayCatActivity extends Activity {
 		setContentView(R.layout.activity_display_cat);
 		bindViews();
 		setListeners();
+
+		final IntentFilter filters = new IntentFilter();
+		filters.addAction("android.net.wifi.WIFI_STATE_CHANGED");
+		filters.addAction("android.net.wifi.STATE_CHANGE");
+		registerReceiver(mReceiver, filters);
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 
-		loadNextCat();
+		if (isConnected())
+			loadNextCat();
 	}
 
 	/**
@@ -51,6 +96,7 @@ public class DisplayCatActivity extends Activity {
 	 */
 	private void bindViews() {
 		mLoadingContainer = (ViewGroup) findViewById(R.id.loading_container);
+		mLoadingText = (TextView) findViewById(R.id.loading_text);
 		mCatImage = (ImageView) findViewById(R.id.cat);
 	}
 
@@ -69,6 +115,7 @@ public class DisplayCatActivity extends Activity {
 
 	private void loadNextCat() {
 		mLoadingContainer.setVisibility(View.VISIBLE);
+		mLoadingText.setText(this.getString(R.string.loading));
 
 		SafeASyncTask<List<Image>> task = new SafeASyncTask<List<Image>>(this) {
 
@@ -110,12 +157,12 @@ public class DisplayCatActivity extends Activity {
 	}
 
 	/**
-	 * We need to keep track of where the user is through the list of cats
-	 * the API is returning
+	 * We need to keep track of where the user is through the list of cats the
+	 * API is returning
 	 * 
-	 * At some point, the API will stop returning lists of cats, but that 
-	 * isn't accounted for in this implementation of the app. With more time,
-	 * it's something that should be considered
+	 * At some point, the API will stop returning lists of cats, but that isn't
+	 * accounted for in this implementation of the app. With more time, it's
+	 * something that should be considered
 	 */
 	private void incrementPosition() {
 		if (mPositionInPage == 20 - 1) {
